58b7bcc4cd2d61b017e27233a048967581c84a67
Paul Wilson <paul@vermonster.com>
2015-06-07 14:57:56 -0400

    Sync to amazon

--- Gemfile
+++ Gemfile
@@ -3,8 +3,14 @@
 source "http://rubygems.org"
 
 gem "middleman"
+gem 'middleman-s3_sync'
 gem "middleman-livereload"
 gem "middleman-deploy"
 gem "bourbon"
 gem "neat"
-gem 'pry'
+
+group :development do
+  gem 'pry'
+  gem 'dotenv'
+end
+
--- config.rb
+++ config.rb
@@ -1,4 +1,6 @@
 require "extensions/views"
+require 'dotenv'
+Dotenv.load
 
 activate :views
 activate :directory_indexes
@@ -38,3 +40,20 @@ helpers do
     link_to(link_text, page_url, options)
   end
 end
+
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket                     = ENV['S3_BUCKET'] # The name of the S3 bucket you are targetting. This is globally unique.
+  s3_sync.region                     = 'us-east-1'     # The AWS region for your bucket.
+  s3_sync.aws_access_key_id          = ENV['S3_KEY']
+  s3_sync.aws_secret_access_key      = ENV['S3_SECRET']
+  s3_sync.delete                     = false # We delete stray files by default.
+  s3_sync.after_build                = false # We do not chain after the build step by default.
+  s3_sync.prefer_gzip                = true
+  s3_sync.path_style                 = true
+  s3_sync.reduced_redundancy_storage = false
+  s3_sync.acl                        = 'public-read'
+  s3_sync.encryption                 = false
+  s3_sync.prefix                     = ''
+  s3_sync.version_bucket             = false
+end
+
